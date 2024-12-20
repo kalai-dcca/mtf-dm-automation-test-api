68de210398c02c44d638eac1bb485d6cc12e7e18
S-HZEYBEK <hzeybek@dcca.com>
2024-12-19 20:29:21 -0600

    Hooks class updated

--- src/main/java/mtf/dm/cms/hhs/gov/cucumber/Hooks.java
+++ src/main/java/mtf/dm/cms/hhs/gov/cucumber/Hooks.java
@@ -2,6 +2,8 @@ package mtf.dm.cms.hhs.gov.cucumber;
 
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import mtf.dm.cms.hhs.gov.report.ExtentReportManager;
 import mtf.dm.cms.hhs.gov.utilities.LoggerUtil;
 
 public class Hooks {
@@ -9,10 +11,12 @@ public class Hooks {
     @Before
     public void setup() {
         LoggerUtil.logger.info("Starting the test...");
+        ExtentReportManager.createTest("Cucumber Test");
     }
 
     @After
     public void tearDown() {
         LoggerUtil.logger.info("Test execution completed.");
+        ExtentReportManager.flush();
     }
 }
