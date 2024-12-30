46e645018125d44cea856d791a51d066b6eba523
rachowdhury-dcca <rachowdhury@dcca.com>
2024-12-30 10:31:11 -0500

    Update db to remove transformations on numerical data

--- src/test.dataExchange/resources/database/dataExchange.sql
+++ src/test.dataExchange/resources/database/dataExchange.sql
@@ -2,12 +2,12 @@
 -- This table stores the primary IPAY value, brand name, and active ingredient name.
 -- Columns:
 --   pk: Auto-incremented primary key.
---   ipay: IPAY value (not nullable).
+--   ipay: IPAY value (double, not nullable).
 --   ndc_brand_name: Selected Drug Name (not nullable).
 --   ndc_active_ingredient_name: Active Ingredient Name (not nullable).
 CREATE TABLE price_ipay_name (
     pk AUTOINCREMENT,
-    ipay TEXT NOT NULL,
+    ipay DOUBLE NOT NULL,
     ndc_brand_name TEXT NOT NULL,
     ndc_active_ingredient_name TEXT NOT NULL,
     PRIMARY KEY (pk)
@@ -18,11 +18,13 @@ CREATE TABLE price_ipay_name (
 -- Columns:
 --   pk: Auto-incremented primary key.
 --   fk_price_ipay_name: Foreign key referencing price_ipay_name(pk).
---   ndc_cd: NDC-11 code (not nullable).
+--   ndc_9: NDC-9 code (not nullable).
+--   ndc_11: NDC-11 code (not nullable).
 CREATE TABLE price_ndc_cd (
     pk AUTOINCREMENT,
     fk_price_ipay_name LONG NOT NULL,
-    ndc_cd TEXT NOT NULL,
+    ndc_9 TEXT NOT NULL,
+    ndc_11 TEXT NOT NULL,
     PRIMARY KEY (pk)
 );
 
@@ -33,9 +35,9 @@ CREATE TABLE price_ndc_cd (
 --   fk_price_ndc_cd: Foreign key referencing price_ndc_cd(pk).
 --   mfp_eff_dt: Effective Date (not nullable).
 --   mfp_end_dt: End Date (nullable).
---   mfp_per_30: Single MFP per 30 DES (currency, not nullable).
---   mfp_per_unit: MFP per Unit Price (currency, not nullable).
---   mfp_per_package: MFP per Package Price (currency, not nullable).
+--   mfp_per_30: Single MFP per 30 DES (double, not nullable).
+--   mfp_per_unit: MFP per Unit Price (double, not nullable).
+--   mfp_per_package: MFP per Package Price (double, not nullable).
 --   asof_dt: As of Date (not nullable).
 --   update_type: Type of Update (not nullable).
 --   remark: Remarks (nullable).
@@ -44,9 +46,9 @@ CREATE TABLE price_eff_dt (
     fk_price_ndc_cd LONG NOT NULL,
     mfp_eff_dt DATE NOT NULL,
     mfp_end_dt DATE,
-    mfp_per_30 CURRENCY NOT NULL,
-    mfp_per_unit CURRENCY NOT NULL,
-    mfp_per_package CURRENCY NOT NULL,
+    mfp_per_30 DOUBLE NOT NULL,
+    mfp_per_unit DOUBLE NOT NULL,
+    mfp_per_package DOUBLE NOT NULL,
     asof_dt DATE NOT NULL,
     update_type TEXT NOT NULL,
     remark TEXT,
@@ -63,24 +65,24 @@ ADD CONSTRAINT fk_price_ndc FOREIGN KEY (fk_price_ndc_cd) REFERENCES price_ndc_c
 
 -- Insert data into price_ipay_name
 INSERT INTO price_ipay_name (ipay, ndc_brand_name, ndc_active_ingredient_name)
-VALUES ('2026', 'ELIQUIS', 'APIXABAN');
+VALUES (2026.0, 'ELIQUIS', 'APIXABAN');
 
 -- Insert data into price_ndc_cd
-INSERT INTO price_ndc_cd (fk_price_ipay_name, ndc_cd)
-VALUES (1, '00003-0893-21');
+INSERT INTO price_ndc_cd (fk_price_ipay_name, ndc_9, ndc_11)
+VALUES (1, '00003-0893', '00003-0893-21');
 
-INSERT INTO price_ndc_cd (fk_price_ipay_name, ndc_cd)
-VALUES (1, '00003-0893-31');
+INSERT INTO price_ndc_cd (fk_price_ipay_name, ndc_9, ndc_11)
+VALUES (1, '00003-0893', '00003-0893-31');
 
-INSERT INTO price_ndc_cd (fk_price_ipay_name, ndc_cd)
-VALUES (1, '00003-0893-41');
+INSERT INTO price_ndc_cd (fk_price_ipay_name, ndc_9, ndc_11)
+VALUES (1, '00003-0893', '00003-0893-41');
 
 -- Insert data into price_eff_dt
 INSERT INTO price_eff_dt (fk_price_ndc_cd, mfp_eff_dt, mfp_end_dt, mfp_per_30, mfp_per_unit, mfp_per_package, asof_dt, update_type, remark)
-VALUES (1, #2026-01-01#, NULL, 231.00, 4.15, 248.70, #2024-08-15#, 'Added', 'Added alongside IPAY2026 MFP announcement');
+VALUES (1, #2026-01-01#, NULL, 231.0, 4.145072, 248.7, #2024-08-15#, 'Added', 'Added alongside IPAY2026 MFP announcement');
 
 INSERT INTO price_eff_dt (fk_price_ndc_cd, mfp_eff_dt, mfp_end_dt, mfp_per_30, mfp_per_unit, mfp_per_package, asof_dt, update_type, remark)
-VALUES (2, #2026-01-01#, NULL, 231.00, 4.15, 414.51, #2024-08-15#, 'Added', 'Added alongside IPAY2026 MFP announcement');
+VALUES (2, #2026-01-01#, NULL, 231.0, 4.145072, 414.51, #2024-08-15#, 'Added', 'Added alongside IPAY2026 MFP announcement');
 
 INSERT INTO price_eff_dt (fk_price_ndc_cd, mfp_eff_dt, mfp_end_dt, mfp_per_30, mfp_per_unit, mfp_per_package, asof_dt, update_type, remark)
-VALUES (3, #2026-01-01#, NULL, 231.00, 4.15, 746.11, #2024-08-15#, 'Added', 'Added alongside IPAY2026 MFP announcement');
+VALUES (3, #2026-01-01#, NULL, 231.0, 4.145072, 746.11, #2024-08-15#, 'Added', 'Added alongside IPAY2026 MFP announcement');
